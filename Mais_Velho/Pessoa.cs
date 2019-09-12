771c2745e9ef916082c44bae7d0d951501049435
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-12 10:03:11 -0300

    Classe Pessoa

---
+++ Mais_Velho/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mais_Velho
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
