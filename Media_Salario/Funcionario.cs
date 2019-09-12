dc5a184d12d26ce0a39bfe9cb75a48b95c0c3eec
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-12 10:11:14 -0300

    Classe funcionário

---
+++ Media_Salario/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Media_Salario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
