2dbc6b78ef18e249c028def52f1553a03d1fda3e
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-12 10:16:53 -0300

    Programa que recebe números e soma

---
+++ Aula1/qtd_numeros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números você vai digitar?");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+                Console.WriteLine("valor " + soma);
+            }
+        }
+	}
+}
