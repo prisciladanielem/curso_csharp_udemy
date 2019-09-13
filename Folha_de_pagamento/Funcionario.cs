76bfed3f530ea055ea2018123b2129337c1b6222
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 14:58:00 -0300

    Programa que realiza os calculos da folha de pagamento de um funcionário

---
+++ Folha_de_pagamento/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Folha_de_pagamento
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Impostos;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Impostos;
+        }
+        public void AumentarSalarioPorcentagem(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto / 100) * porcentagem;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + " $ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Folha_de_pagamento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Folha_de_pagamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario();
+
+            Console.Write("Informe o nome do funcionário: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Informa o salário bruto do funcionário: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Informe o valor dos impostos que serão descontados:");
+            funcionario.Impostos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.SalarioLiquido();
+
+            Console.WriteLine("Funcionário: " + funcionario);
+
+            Console.WriteLine();
+            Console.Write("Informa a porcentagem de aumento: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalarioPorcentagem(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + funcionario);
+        }
+    }
+}
