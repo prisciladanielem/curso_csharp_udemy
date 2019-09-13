6c83247f2389013219e2a974cca6372cc93e54ef
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 13:44:53 -0300

    Classe principal

---
+++ CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class ConversorDeMoeda
+    {
+        private static double IOF = 6.0;
+
+        public static double ConverteDolarEmReais(double CotacaoDolar, double ValorComprasDolar)
+        {  
+            double total = ValorComprasDolar + (ValorComprasDolar / 100) * IOF;
+            return CotacaoDolar * total;
+        }
+    }
+}
