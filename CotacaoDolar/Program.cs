808abde1074957d4c8bd5d2b73aac455eb40f972
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 13:46:29 -0300

    Programa princial

---
+++ CotacaoDolar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Qual a cotação do dólar? ");
+            double valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantiaDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.ConverteDolarEmReais(valorDolar, quantiaDolar).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
