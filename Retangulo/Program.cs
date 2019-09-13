f6de0a242f652a8a38597cdad6c44b1a1e1a2c56
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 15:00:30 -0300

    Programa que realiza o calculo da área, perimetro e a diagonal de um retangulo

---
+++ Retangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retangulo = new Retangulo();
+
+            Console.Write("Digite a largura do retangulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a altura do retangulo: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + retangulo.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
