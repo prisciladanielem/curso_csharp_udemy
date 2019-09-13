a4a773541acc33f1d573029d26744049bd3e86d7
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 15:03:14 -0300

    Programa que calcula a circunferencia de um Circulo

---
+++ CalculadoraCircunferencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraCircunferencia
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
