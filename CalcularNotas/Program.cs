10dd70dc2451adefbf7f667ea38837630261c824
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-13 16:59:41 -0300

    Programa que recebe notas e retorna se o aluno foi aprovado ou não

---
+++ CalcularNotas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcularNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno = new Aluno();
+            Console.Write("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas dos alunos: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota final = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram " + aluno.NotaRestante().ToString("F1", CultureInfo.InvariantCulture) + " pontos");
+            }
+        }
+    }
+}
