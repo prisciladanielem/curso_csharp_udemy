4a0dd0223a69e5454be37e5457cad05b66fa1395
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-16 14:52:45 -0300

    Classe do que gerencia o aluguel

---
+++ Quartos/Aluguel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quartos
+{
+    class Aluguel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Aluguel(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
