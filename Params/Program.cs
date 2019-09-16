10fd61b1dc678fb1ac1b839236d1f9e5ac44eb00
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-16 14:57:57 -0300

    Programa que ilustra o uso do params

---
+++ Params/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = Calculator.Sum(2, 5);
+            Console.WriteLine(soma);
+        }
+    }
+}
