c85b92c77b207e85c1b270e12c4877adc5cdddf9
Priscila Moreira <prisciladanielem@gmail.com>
2019-09-16 14:53:48 -0300

    Programa que realiza o gerenciamento de um hotel

---
+++ Quartos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Quartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many rooms will be rented?");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Aluguel[] vect = new Aluguel[9];
+
+            for(int i = 0; i < qtd; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Aluguel (nome, email);
+            }
+
+            Console.WriteLine("Busy rooms");
+            for(int i = 0; i < vect.Length;  i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                } 
+            }
+        }
+    }
+}
